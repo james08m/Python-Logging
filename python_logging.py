80913acc1a05e47306bac4771b683485f380c5ea
james08m <james08major@gmail.com>
2018-12-09 14:33:57 -0500

    Update python_logging comments and remove unused code lines.

--- python_logging.py
+++ python_logging.py
@@ -15,34 +15,36 @@ import datetime
 import time
 
 
+# Get actual time and properly format it into strings
 actual_time = datetime.datetime.now()
-str_date_time = actual_time.strftime("%Y-%m-%d %H:%M:%S")
+#str_date_time = actual_time.strftime("%Y-%m-%d %H:%M:%S")
 str_date = actual_time.strftime("%Y-%m-%d")
-log_format = "%(asctime)s\t%(name)s\t%(levelname)s\t\t%(message)s" # formatter string uses %(<dictionary key>)s styled string substitution; the possible keys are documented in LogRecord attributes.
+
+# String format of our logs
+# Formatter,used bellow , uses %(<dictionary key>)s styled string substitution; the possible keys are documented in LogRecord attributes.
+log_format = "%(asctime)s\t%(name)s\t%(levelname)s\t\t%(message)s"
 
 # Create logger object
 logger = logging.getLogger("LOGTEST")
-logging.basicConfig(format=log_format, filename=str_date) # Pass file name and custom log format for the file
-logger.setLevel(logging.DEBUG)                            # Set level to display debug and high level message
+logging.basicConfig(format=log_format, filename=str_date) # Pass the filename and the log strings format used in file logs
+logger.setLevel(logging.DEBUG)                            # Set logging level to display debug and higher level in the file
 
-# Create console handler to display log on console and not just on file
+# Create console handler to display log on the console and not just in file
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)                   # Can select a different lvl for the console if needed
+console_handler.setLevel(logging.DEBUG)                   # Can select a different logging lvl for the console if needed
 
-# create formatter
-formatter = logging.Formatter(log_format)                 # Can select a different format for the console if needed
+# Create formatter
+formatter = logging.Formatter(log_format)                 # Can select a different log strings format for the console if needed
 
-# add formatter to ch
+# Add the formatter to console handler
 console_handler.setFormatter(formatter)
 
-# add ch to logger
+# Add the console handler to logger
 logger.addHandler(console_handler)
 
-# 'application' code
+# How to use it
 logger.debug("This is a simple debug message")
 logger.info("This is a simple informative message")
 logger.warning("This is a message to remember that you have to be careful over here")
 logger.error("This message is to let you know that something went wrong")
 logger.critical("This message is to let you know that you f*ck up really bad!")
-
-time.sleep(10)
